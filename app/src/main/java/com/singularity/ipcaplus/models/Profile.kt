3dfa015645d38e15cdb7e45ddc9646c252c8c8b6
Vitoo1408 <77049597+Vitoo1408@users.noreply.github.com>
2021-12-27 11:51:25 +0000

    Profile Data Model Update

--- app/src/main/java/com/singularity/ipcaplus/models/Profile.kt
+++ app/src/main/java/com/singularity/ipcaplus/models/Profile.kt
@@ -9,15 +9,17 @@ class Profile {
     var course : String = ""
     var year : String = ""
     var age : Int = 0
+    var role : String = ""
     var id : String? =  null
 
-    constructor(user: String, studentNumber: String, contact: String, course: String, year: String, age : Int) {
+    constructor(user: String, studentNumber: String, contact: String, course: String, year: String, age : Int, role : String) {
         this.name = user
         this.studentNumber = studentNumber
         this.contact = contact
         this.course = course
         this.year = year
         this.age = age
+        this.role = role
     }
 
     fun toHash() :HashMap<String, Any> {
@@ -28,6 +30,7 @@ class Profile {
         hashMap.put("course", course)
         hashMap.put("year", year)
         hashMap.put("age", age)
+        hashMap.put("role", role)
         return hashMap
     }
 
@@ -39,7 +42,8 @@ class Profile {
                 hashMap["contact"] as String,
                 hashMap["course"] as String,
                 hashMap["year"] as String,
-                hashMap["age"] as Int
+                hashMap["age"] as Int,
+                hashMap["role"] as String
             )
             return profile
         }
