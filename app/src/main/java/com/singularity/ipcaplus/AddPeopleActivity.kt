6d979b778eec880c25e079e6aeea25bc62595e09
Vitoo1408 <77049597+Vitoo1408@users.noreply.github.com>
2022-01-02 00:22:13 +0000

    COMMIT DE UMA PALAVRA

--- app/src/main/java/com/singularity/ipcaplus/AddPeopleActivity.kt
+++ app/src/main/java/com/singularity/ipcaplus/AddPeopleActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.singularity.ipcaplus.chat.ChatActivity
