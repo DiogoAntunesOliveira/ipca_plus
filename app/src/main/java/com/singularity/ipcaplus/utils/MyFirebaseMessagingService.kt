a6d8c953408727a74dd3deed262b359e6f4a7f56
N. Isidoro <80036723+NunoIsidoro@users.noreply.github.com>
2022-01-05 15:38:21 +0000

    add parte web

--- app/src/main/java/com/singularity/ipcaplus/utils/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/singularity/ipcaplus/utils/MyFirebaseMessagingService.kt
@@ -18,9 +18,23 @@ import java.lang.Exception
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
+    // [START on_new_token]
+    /**
+     * Called if the FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+
+    }
+    // [END on_new_token]
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-
-
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: ${remoteMessage.from}")
@@ -65,21 +79,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     // [END receive_message]
 
-    // [START on_new_token]
-    /**
-     * Called if the FCM registration token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the
-     * FCM registration token is initially generated so this is where you would retrieve the token.
-     */
-    override fun onNewToken(token: String) {
-        Log.d(TAG, "Refreshed token: $token")
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
-        sendRegistrationToServer(token)
-    }
-    // [END on_new_token]
 
     /**
      * Schedule async work using WorkManager.
