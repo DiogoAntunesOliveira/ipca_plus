798f106412ef518079d85b605125bcaa93562f6a
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2022-01-11 18:40:41 +0000

    extra

---
+++ ipca_plus_backoffice/js/utilis.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("list");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByClassName("item-info")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
