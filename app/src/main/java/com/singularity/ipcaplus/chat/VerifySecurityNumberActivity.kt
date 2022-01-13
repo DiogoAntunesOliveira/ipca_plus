7238bf15e30a41e60ef38479209945050c4f1f4e
Diogo Antunes Oliveira <diogojose11@live.com>
2022-01-13 19:24:20 +0000

    Blockchain completed verification button

--- app/src/main/java/com/singularity/ipcaplus/chat/VerifySecurityNumberActivity.kt
+++ app/src/main/java/com/singularity/ipcaplus/chat/VerifySecurityNumberActivity.kt
@@ -2,31 +2,31 @@ package com.singularity.ipcaplus.chat
 
 import android.annotation.SuppressLint
 import android.app.ActionBar
-import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import com.google.android.material.snackbar.Snackbar
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.qrcode.QRCodeWriter
-import com.google.zxing.qrcode.encoder.QRCode
-import com.singularity.ipcaplus.LoginActivity
 import com.singularity.ipcaplus.R
 import com.singularity.ipcaplus.cryptography.getMetaOx
+import com.singularity.ipcaplus.databinding.ActivityDrawerActivtyBinding
+import com.singularity.ipcaplus.databinding.ActivityRegisterBinding
+import com.singularity.ipcaplus.databinding.ActivityVerifySecurityNumberBinding
 import com.singularity.ipcaplus.utils.Backend.put0xBlank
 import com.singularity.ipcaplus.utils.Backend.put0xBlankProfile
+import kotlinx.coroutines.*
 
 class VerifySecurityNumberActivity : AppCompatActivity() {
 
@@ -34,7 +34,7 @@ class VerifySecurityNumberActivity : AppCompatActivity() {
     private lateinit var keygenHash: String
     private lateinit var verifySecurity: Button
     private lateinit var hexaHashKey: TextView
-    private lateinit var mQrResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var binding: ActivityVerifySecurityNumberBinding
 
 
     @SuppressLint("WrongConstant")
@@ -42,6 +42,11 @@ class VerifySecurityNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify_security_number)
+
+        binding = ActivityVerifySecurityNumberBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.custom_bar_layout)
         findViewById<TextView>(R.id.AppBarTittle).text = "Verificação de Segurança"
@@ -107,10 +112,20 @@ class VerifySecurityNumberActivity : AppCompatActivity() {
             // Starts scanner on Create of Overlay (you can also call this function using a button click)
             initScanner()
         }
+
         findViewById<Button>(R.id.verifySecurityButton).setOnClickListener{
             put0xBlank(chat_id)
-            put0xBlankProfile(chat_id){
-                finish()
+            put0xBlankProfile(chat_id){}
+            GlobalScope.launch {
+                withContext(Dispatchers.IO){
+                    Snackbar.make(binding.root,
+                        "A tua rende foi validada", Snackbar.LENGTH_SHORT).show()
+                    delay(1000)
+                    Snackbar.make(binding.root,
+                        "Bem vindo a WEB 3.0", Snackbar.LENGTH_SHORT).show()
+                    delay(2000)
+                    finish()
+                }
             }
         }
 
